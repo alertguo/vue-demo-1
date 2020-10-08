720897ea4dbadd2da1df82e75c2f0504786dad88
alertguo <1196614612@qq.com>
2020-10-08 16:22:46 +0800

    init

---
+++ src/Demo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="red">
+    {{ n }}
+    <button @click="add">+1</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      n: 0,
+    }
+  },
+  created(){
+    console.log('实例创建了')
+  },
+  mounted(){
+    console.log('实例出现在页面了')
+  },
+  updated(){
+    console.log('实例更新了')
+  },
+  destroyed(){
+    console.log('实例消亡了')
+  },
+  methods: {
+    add() {
+      this.n += 1
+    },
+  }
+}
+</script>
+<style scoped>
+.red {
+  color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,74 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+const Vue = window.Vue
+// 默认生成的
+// import Vue from 'vue'
+// import App from './App.vue'
+//
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+// 完整版
+// new Vue({
+//   el:'#app',
+//   template:`<div>
+//     {{n}}
+//     <button @click="add">+1</button>
+//     </div>`,
+//   data:{
+//     n:0
+//   },
+//   methods:{
+//     add(){
+//       this.n += 1
+//     }
+//   }
+// })
+
+// 运行时
+// new Vue({
+//   el: '#app',
+//   render(h) {
+//     return h('div', [this.n, h('button', {on: {click: this.add}}, '+1')])
+//   },
+//   data: {
+//     n: 0
+//   },
+//   methods: {
+//     add() {
+//       this.n += 1
+//     }
+//   }
+// })
+
+// 单文件组件
+// import Demo from './Demo.vue'
+//
+// new Vue({
+//   el: '#app',
+//   render(h) {
+//     return h(Demo)
+//   }
+// })
+import Demo from './Demo.vue'
 new Vue({
-  render: h => h(App),
+  components:{Demo},
+  data(){
+    return{
+      visible: true
+    }
+  },
+  template:`
+    <div>
+      <button @click="toggle">toggle</button>
+      <hr>
+      <Demo v-if="visible === true"/>
+    </div>
+  `,
+  methods:{
+    toggle(){
+      this.visible = !this.visible
+    }
+  }
 }).$mount('#app')
